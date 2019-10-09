1c077dc781f028bfbd75376e9bc3fd8ecb04b8ed
NecroticMage <jakob.meyer.git@gmail.com>
2019-10-09 23:27:51 +0200

    a

---
+++ Unity Projekt/first-game/Assets/Scrips/CamController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public GameObject camBind;
+    public Transform camTransform;
+    private Transform pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = camBind.transform;
+        camTransform = pos;
+        
+    }
+}
+
+//this.AddForce(1,1,1)
+//Input.getKey("d")
---
+++ Unity Projekt/first-game/Assets/Scrips/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody player;
+    public float speed;
+    public float forwardModify;
+    public float jumpHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("w"))
+        {
+            player.AddForce(0, 0, speed * forwardModify * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            player.AddForce(0, 0, speed * Time.deltaTime *-1);
+        }
+        if (Input.GetKey("d"))
+        {
+            player.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            player.AddForce(speed * Time.deltaTime * -1, 0, 0);
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            player.AddForce(0, jumpHeight, 0);
+        }
+    }
+}
